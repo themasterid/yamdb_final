d9999bddb9641ff60db063e997bf8ca9e0390662
themasterid <thebrootos@gmail.com>
2022-03-08 23:06:24 +0500

    mb fix 2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,10 +1,6 @@
 import os
 from datetime import timedelta
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = (
