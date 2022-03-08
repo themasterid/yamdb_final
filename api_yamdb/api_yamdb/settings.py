c6638f8f27aaace16bf7837a7d687228e992e1c0
themasterid <thebrootos@gmail.com>
2022-03-08 17:14:52 +0500

    small fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,8 +1,6 @@
 import os
 from datetime import timedelta
 
-from dotenv import load_dotenv
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = (
@@ -23,8 +21,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
-    'reviews',
     'api',
+    'reviews',
 ]
 
 MIDDLEWARE = [
