caca89ef5ff7260c788b56a170e8d87ddad3b967
themasterid <thebrootos@gmail.com>
2022-03-08 20:24:07 +0500

    lets go 08.03

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -63,15 +63,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
-DATABASES = { 
-    'default': { 
+DATABASES = {
+    'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default=5432)
-    } 
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
