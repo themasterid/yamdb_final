1aad490ab394900371429c80f15988ddd916de14
Sourcery AI <>
2023-03-24 12:34:42 +0000

    'Refactored by Sourcery'

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -19,9 +19,7 @@ class IsAdminUserOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if request.user.is_authenticated:
-            return request.user.is_admin
-        return False
+        return request.user.is_admin if request.user.is_authenticated else False
 
 
 class AdminModeratorAuthorPermission(permissions.BasePermission):
